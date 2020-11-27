52dacdfcae501333e68bab267be7bcf86383ec3b
JuanDuran85 <duran85juan@gmail.com>
2020-11-27 11:06:31 -0300

    trabajando con hook y apis

---
+++ hooks_apis/src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <HelloWorld/>
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'App',
+  data() {
+    return {
+      datosApi: []
+    }
+  },
+  components: {
+    HelloWorld
+  },
+  created() {
+    fetch('https://reqres.in/api/users')
+    .then(resp => resp.json())
+    .then(result => console.log(result))
+    .catch(error => console.error(error));
+  }
+}
+</script>
+
+<style>
+
+</style>
