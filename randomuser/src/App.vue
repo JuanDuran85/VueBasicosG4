7793eadf65a7ec3680ab862c06f4170230c14759
JuanDuran85 <duran85juan@gmail.com>
2020-11-30 13:04:46 -0300

    generando usario aleatorio con calses dinamicas y fetch

---
+++ randomuser/src/App.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+    <Usuarios :user="usuario"/>
+  </div>
+</template>
+
+<script>
+import Usuarios from './components/Usuarios.vue'
+
+export default {
+  name: 'App',
+  data() {
+    return {
+      usuario: []
+    }
+  },
+  components: {
+    Usuarios
+  },
+  created() {
+    fetch('https://randomuser.me/api/')
+    .then(resp => resp.json())
+    .then(data =>{
+      console.log(data.results);
+      this.usuario = data.results;
+    })
+    .catch(error => console.error(error));
+  },
+}
+</script>
+
+<style>
+
+</style>
---
+++ randomuser/src/components/Usuarios.vue
@@ -0,0 +1,49 @@
+<template>
+  <div class="container text-center mt-4">
+    <h1>Información del Usuario</h1>
+    <div class="mt-5" v-for="(item,index) in user" :key="index">
+      <div class="card" :class="[(item.gender == 'female') ? 'female' : 'male']">
+        <img :src="item.picture.large" class="card-img-top" :alt="item.id.name">
+        <div class="card-body">
+          <h5 class="card-title">{{fullName}}</h5>
+          <p class="card-text">{{item.location.timezone.description}}</p>
+        </div>
+        <ul class="list-group list-group-flush">
+          <li class="list-group-item">Email: {{item.email}}</li>
+          <li class="list-group-item">Phone: {{item.phone}}</li>
+          <li class="list-group-item">Username: {{item.login.username}}</li>
+        </ul>
+        <div class="card-body">
+          <a href="#" class="card-link">Card link</a>
+          <a href="#" class="card-link">Another link</a>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Usuarios',
+  props: {
+    user: {
+      type: Array,
+      required: true,
+    }
+  },
+  computed: {
+    fullName(){
+      return this.user[0].name.title + " " + this.user[0].name.first + " " + this.user[0].name.last
+    }
+  },
+}
+</script>
+
+<style scoped>
+  .female{
+    background-color: fuchsia;
+  }
+  .male{
+    background-color: blue;
+  }
+</style>
